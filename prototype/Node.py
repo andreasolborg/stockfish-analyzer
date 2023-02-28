54ea2c7fc2e6f9de85d763212069c394a3e495ad
Jon <jon.grendstad@gmail.com>
2023-02-28 09:09:30 +0100

    more planning for openings

--- prototype/Node.py
+++ prototype/Node.py
@@ -1,6 +1,20 @@
 class Node:
     '''
-    Encapsulate all games parsed from a PGN file.
+    A class representing a node in the chess opening tree.
     '''
-    def __init__(self, color):
-        self.color = color
+    def __init__(self, color, children, meta_data):
+        '''
+        Params:
+            color: Either the character 'w' for white or 'b' for black
+            games: A list with instances of PGNGame
+        '''
+        self.move # string of move, acts like as ID
+        self.degree # move number
+        self.color = color # w/b
+        self.children = children # list of all moves done from this
+        self.meta_data = meta_data 
+    
+    def add_child(self):
+        pass
+    
+    
--- prototype/Tree.py
+++ prototype/Tree.py
@@ -1,14 +1,66 @@
+'''
+# PLAN
+
+### Task 9: 
+- Each position is only stored once in the chess tree. This means that regardless of the moves played to reach it, the exact same information will be stored
+    - The number of games won by white in this position
+    - The number of games win by black in this position
+    - The number of games resulted in a draw in this position
+    - The name of the opening
+    - The move
+    - The children nodes
+    - The degree (move number)
+    
+### Task 10: 
+- We use PGNDatabase functions and send a list of games into this class as a argument
+    - For example, get_games_where_stockfish_is_black()
+- TODO: how do we parse this game an create a tree??
+
+root = Node(color, children, meta_data)
+node2 = Node(color2, children2, meta_data2)
+node3 = Node(...)
+node4 = Node(...)
+node5 = Node(...)
+node6 = Node(...)
+
+root.add_child(node2)
+root.add_child(node3)
+node2.add_child(node4)
+node2.add_child(node5)
+node3.add_child(node6)
+
+tree = Tree(root)
+tree.visualize()
+
+### Task 11: 
+- Depth is number of half moves
+- We print the opening tree using graphviz
+
+### Task 12
+
+'''
+import graphviz
+
 class Tree:
     '''
     TODO: write class desc
     '''
-    def __init__(self, color, games):
+    def __init__(self, player_color, games):
         '''
-        Params
-            color: Either the character 'w' for white or 'b' for black
+        Params:
+            player_color: Either 'w' or 'b'. This is the color of the player we want to analyze openings for
             games: A list with instances of PGNGame
         '''
-        self.color = color
+        self.nodes = [] # When we parse self.games we should get this filled up
+        self.player_color = player_color
+        self.root_node = root_node # We must choose the root node we want? Or do we create a three starting from no move?
         self.games = games
+        
+        
+    def create_tree(self):
+        pass
+    
+    def visualize(self):
+        pass
         
     
