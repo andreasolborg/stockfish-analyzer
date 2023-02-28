7ef28ac64e72114ae292e24605b4c3cb8704f27c
Jon <jon.grendstad@gmail.com>
2023-02-28 22:07:40 +0100

    save2

--- prototype/Node.py
+++ prototype/Node.py
@@ -2,17 +2,16 @@ class Node:
     '''
     A class representing a node in the chess opening tree.
     '''
-    def __init__(self, color, children, meta_data):
+    def __init__(self, moves_token):
         '''
         Params:
             color: Either the character 'w' for white or 'b' for black
             games: A list with instances of PGNGame
         '''
-        self.move # string of move, acts like as ID
-        self.degree # move number
-        self.color = color # w/b
+        
+        self.moves_token = moves_token   
         self.children = children # list of all moves done from this
-        self.meta_data = meta_data 
+
     
     def add_child(self):
         pass
--- prototype/Tree.py
+++ prototype/Tree.py
@@ -41,79 +41,114 @@ tree.visualize()
 
 '''
 
-import pydot
+
+
+import time
 from PGNDatabase import PGNDatabase
+from anytree import Node, RenderTree
+
+class Node:
+    '''
+    A class representing a node in the chess opening tree.
+    '''
+    def __init__(self, moves_token):
+        '''
+        Params:
+            color: Either the character 'w' for white or 'b' for black
+            games: A list with instances of PGNGame
+        '''
+        
+        self.moves_token = moves_token   
+        self.children = []
+        self.count = 1
+
+    def get_moves_token(self):
+        return self.moves_token
+    
+    def add_child(self, node):
+        self.children.append(node)
+    
+    def get_all_children(self):
+        return self.children
+    
+    def increase_count(self):
+        self.count += 1
+
 
 class Tree:
     '''
     TODO: write class desc
     '''
-    def __init__(self, games):
+    def __init__(self, database):
         '''
         Params:
-            player_color: Either 'w' or 'b'. This is the color of the player we want to analyze openings for
-            games: A list with instances of PGNGame
+     
         '''
-        self.nodes = [] # When we parse self.games we should get this filled up
-        # self.root_node = root_node # We must choose the root node we want? Or do we create a three starting from no move?
-        self.games = games
+        self.database = database
+        self.root_node = Node("")
+        self.node_lookup = {}
+        self.add_node_to_lookup(self.root_node)
         
-        
-    def create_tree(self):
+    def get_node_by_move_token(self):
         pass
     
-    def visualize(self):
-        pass
+    def add_node_to_lookup(self, node):
+        self.node_lookup[node.get_moves_token()] = node
+    
+    def get_node_from_lookup(self, moves_token):
+        return self.node_lookup[moves_token]
+    
+    def create_white_tree(self, depth):
+        games = self.database.get_games_where_stockfish_is_white()
+        
+        for i in range(depth):
+            #print("####",i, "####")
+            
+            for game in games:
+                parent_moves_token = ""
+                new_moves_token = ""
+                
+                # this will create a string token for the parent and the new node
+                for j in range(i+1):
+                    if j != i:
+                        parent_moves_token += game.get_half_move(j) + " "
+                    new_moves_token += game.get_half_move(j) + " "
+                
+                # if the sequence of moves does not exist as a node we create a new node and add it to the children of its parent
+                if new_moves_token not in self.node_lookup:
+                    new_node = Node(new_moves_token)
+                    self.add_node_to_lookup(new_node)
+                    self.get_node_from_lookup(parent_moves_token).add_child(new_node)
+                # if the node exist we can add meta data etc to it
+                else:
+                    
+                    ## legg til win/tap/draw på eksiterende node
+                    
+                    self.get_node_from_lookup(new_moves_token).increase_count()
+            
+    
+    def visualize_tree(self):
+        for pre, _, node in RenderTree(self.root_node):
+            print(f"{pre}{node.moves_token}")
     
 
 def main():
-    #pgn = PGNDatabase("sample.pgn")
-    #tree = Tree(pgn)
-
-    # Anytree package
-    #from anytree import Node, RenderTree
-    #from anytree.exporter import DotExporter
+    start_time = time.time()
+    pgn = PGNDatabase("sample.pgn")
+    
+    tree = Tree(pgn)
+    tree.create_white_tree(20)
+    tree.visualize_tree()
+    
+    #print(len(tree.get_node_from_lookup("").get_all_children()))
+    #print(len(tree.get_node_from_lookup("e4 c5 Nf3 ").get_all_children()))
+    #print(len(tree.get_node_from_lookup("e4 c5 Nf3 d6 ").get_all_children()))
+    #print(len(tree.get_node_from_lookup("e4 c5 Nf3 d6 d4 cxd4 Nxd4 Nf6 Nc3 a6 Bg5 e6 f4 Nbd7 Qf3 Qc7 O-O-O b5 ").get_all_children()))
     
     
-    #for game in pgn.get_games():
-    #    parent_node = Node(game.get_move(0))
-    #    for move in game.get_moves():
-    #        child = Node(move, parent=parent_node)
-    #        parent_node = child
-        
-        
-
-        
-    #udo = Node("Udo")
-    #marc = Node("Marc", parent=udo)
-    #lian = Node("Lian", parent=marc)
-    #dan = Node("Dan", parent=udo)
-    #jet = Node("Jet", parent=dan)
-    #jan = Node("Jan", parent=dan)
-    #joe = Node("Joe", parent=dan)
-    #print(udo)
-    #DotExporter(udo).to_picture("udo.png")
+    print(f"Time: {time.time() - start_time}")
 
 
-
-    ## pydot package
-    
-    # graph = pydot.Dot("my_graph", graph_type="graph", bgcolor="white")
-    # # Add nodes
-    # my_node = pydot.Node("a", label="Foo")
-    # graph.add_node(my_node)
-    # # Or, without using an intermediate variable:
-    # graph.add_node(pydot.Node("b", shape="circle"))
-
-    # # Add edges
-    # my_edge = pydot.Edge("a", "b", color="blue")x
-    # graph.add_edge(my_edge)
-    # # Or, without using an intermediate variable:
-    # graph.add_edge(pydot.Edge("b", "c", color="blue"))
-
-    # graph.write_png("output.png")
-
-if __name__ == '__main__':
-    main()
+main()
         
     
