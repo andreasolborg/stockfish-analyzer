b7e0649a8ffe85e62b9e49ab2b9a0e3dd6a4959d
Jon <jon.grendstad@gmail.com>
2023-03-07 17:48:49 +0100

    added example of how we add a link to the full picture on the system

--- app/PGNDocument.py
+++ app/PGNDocument.py
@@ -5,6 +5,8 @@ from docx import Document
 from docx.shared import Inches
 from PGNDatabase import PGNDatabase
 from Tree import *
+import docx
+from docx.enum.dml import MSO_THEME_COLOR_INDEX
 
 class PGNDocument:
     '''
@@ -117,7 +119,34 @@ class PGNDocument:
 
         self.create_document_section_for_tree_plotting()
 
+    def add_hyperlink(self, paragraph, text, url):
+    # This gets access to the document.xml.rels file and gets a new relation id value
+        part = paragraph.part
+        r_id = part.relate_to(url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True)
 
+        # Create the w:hyperlink tag and add needed values
+        hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
+        hyperlink.set(docx.oxml.shared.qn('r:id'), r_id, )
+
+        # Create a w:r element and a new w:rPr element
+        new_run = docx.oxml.shared.OxmlElement('w:r')
+        rPr = docx.oxml.shared.OxmlElement('w:rPr')
+
+        # Join all the xml elements together add add the required text to the w:r element
+        new_run.append(rPr)
+        new_run.text = text
+        hyperlink.append(new_run)
+
+        # Create a new Run object and add the hyperlink into it
+        r = paragraph.add_run ()
+        r._r.append (hyperlink)
+
+        # A workaround for the lack of a hyperlink style (doesn't go purple after using the link)
+        # Delete this if using a template that has the hyperlink style in it
+        r.font.color.theme_color = MSO_THEME_COLOR_INDEX.HYPERLINK
+        r.font.underline = True
+
+        return hyperlink
 
 
     ############ TREE PLOTTING ############################
@@ -129,6 +158,8 @@ class PGNDocument:
         self.document.add_heading('3.1 Sicilian defence', level=2)
         list_of_games = self.database.get_games_with_opening("Sicilian defence")
         save_tree_from_list_of_games(list_of_games, 10, "tree_Sicilian")
+        p = self.document.add_paragraph('')
+        self.add_hyperlink(p, 'open full picture', "./graphs/tree_Sicilian.png")
         self.document.add_picture("./graphs/tree_Sicilian.png", width=Inches(8.0))
 
         self.document.add_heading('3.2 French defence', level=2)
@@ -292,7 +323,7 @@ def main():
     database = PGNDatabase("./databases/Stockfish_15_64-bit.commented.[2600].pgn")
     opening_occurrences = 40
     document = PGNDocument(database, opening_occurrences)
-    
+
 
 
     print("Time elapsed: " + (str(time.time() - start_time)) + " seconds")
--- environment_requirements.md
+++ environment_requirements.md
@@ -2,3 +2,5 @@
 2. install graphviz trough web browser
 3. add graphviz to system path
 4. conda install -c conda-forge python-docx
+
+
