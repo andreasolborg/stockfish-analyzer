8a85170a99c93b2564d736d35b1d7fca95132f12
andreasolborg <andreas.olborg@hotmail.no>
2023-03-09 19:26:02 +0100

    b

--- app/Document.py
+++ app/Document.py
@@ -327,6 +327,7 @@ def main():
     ## Todo: tree depth as input parameter.
     ## If no depth is specified, we need to check if the occurences of the opening is above a certain threshold. If it is, we need to go deeper in the tree. Fixed size
     ## Make main file
+    ## We need to be able plot trees even though they are under opening occurences threshold.
 
     start_time = time.time()
     database = PGNDatabase()
